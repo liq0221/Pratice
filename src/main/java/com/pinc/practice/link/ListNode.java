47876da5cc1f4d6ee1e69e0efed48a2ebd87722a
liqian <469363040@qq.com>
2021-09-13 23:58:02 +0800

    feat:链表

---
+++ src/main/java/com/pinc/practice/link/ListNode.java
@@ -0,0 +1,64 @@
+package com.pinc.practice.link;
+
+public class ListNode {
+
+    private Node head;
+
+    private Node last;
+
+    private int size;
+
+    public void insert(int data, int index) {
+        if (index < 0 || index > size) {
+            throw new RuntimeException("位置超出范围");
+        }
+        Node insertNode = new Node(data);
+        if (size == 0) {
+            head =  insertNode;
+            last = insertNode;
+        } else if (index == size) {
+            last.next = insertNode;
+            last = insertNode;
+        } else {
+            Node preNode = get(index - 1);
+            Node nextNode = preNode.next;
+            preNode.next = insertNode;
+            insertNode.next = nextNode;
+        }
+        size++;
+    }
+
+    private Node get(int index) {
+        int i = 0;
+        Node temp = head;
+        while (i < index) {
+            temp = temp.next;
+            i++;
+        }
+        return temp;
+    }
+
+    public void output() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+        System.out.println(size);
+    }
+
+    private static class Node {
+
+        private int data;
+
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/com/pinc/practice/link/Main.java
@@ -0,0 +1,15 @@
+package com.pinc.practice.link;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode();
+        listNode.insert(3, 0);
+        listNode.insert(7, 1);
+        listNode.insert(9, 2);
+        listNode.insert(5, 3);
+        listNode.insert(6, 1);
+        listNode.insert(8, 4);
+        listNode.output();
+    }
+}
