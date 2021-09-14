f006e8cbfd59caf3a7c17b8ad30d6bf4ece68640
liqian <469363040@qq.com>
2021-09-14 22:38:56 +0800

    feat:swap新写法

--- src/main/java/com/pinc/practice/sort/Sort.java
+++ src/main/java/com/pinc/practice/sort/Sort.java
@@ -11,9 +11,17 @@ public abstract class Sort {
     abstract void sort();
 
     public void swap(int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+//        int temp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = temp;
+        // 例：arr[i] = 甲 arr[j] = 乙
+        // arr[i] = 甲 ^ 乙
+        // arr[j] = 甲 ^ 乙 ^ 乙 = 甲
+        // arr[i] = 甲 ^ 乙 ^ 甲 = 乙
+        // ^(异或)：同位相加不进位
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
     }
 
     public void output() {
