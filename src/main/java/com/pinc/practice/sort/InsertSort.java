d41579e66f9dd06493a151811e23d4503fd6e8f9
liqian <liqian@ccbscf.com>
2021-09-23 10:13:46 +0800

    feat:插入排序

---
+++ src/main/java/com/pinc/practice/sort/InsertSort.java
@@ -0,0 +1,24 @@
+package com.pinc.practice.sort;
+
+public class InsertSort extends Sort{
+
+    private int[] arr;
+
+    public InsertSort(int[] arr) {
+        super(arr);
+        this.arr = arr;
+    }
+
+    @Override
+    public void sort() {
+        int insertValue;
+        for (int i = 1; i < arr.length; i++) {
+            insertValue = arr[i];
+            int j = i - 1;
+            for (; (j > 0 && arr[j] > insertValue); j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = insertValue;
+        }
+    }
+}
--- src/main/java/com/pinc/practice/sort/Main.java
+++ src/main/java/com/pinc/practice/sort/Main.java
@@ -4,13 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {2, 5, 11, 8, 4, 9};
-        System.out.println("============选择排序============");
-        Sort s = new SelectorSort(arr);
-        s.sort();
-        s.output();
-        System.out.println("============冒泡排序============");
-        Sort s1 = new BubbleSort(arr);
-        s1.sort();
-        s1.output();
+//        System.out.println("============选择排序============");
+//        Sort s = new SelectorSort(arr);
+//        s.sort();
+//        s.output();
+//        System.out.println("============冒泡排序============");
+//        Sort s1 = new BubbleSort(arr);
+//        s1.sort();
+//        s1.output();
+        System.out.println("============插入排序============");
+        Sort s2 = new BubbleSort(arr);
+        s2.sort();
+        s2.output();
     }
 }
